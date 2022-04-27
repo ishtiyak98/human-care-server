bd4a1ff11603ae8a5f6af3ad6cbe916ceefa7589
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-27 13:10:56 +0600

    all events api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
 const app = express();
@@ -13,15 +14,31 @@ app.use(cors());
 //bZvIbcEAq0YHrbfR
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://humanCare:<password>@cluster0.z2j3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.z2j3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+  try{
+    await client.connect();
+    const eventsCollection = client.db("humanCare").collection("events");
+
+    //!------- Show ALL events ---------
+    app.get("/events", async(req, res)=>{
+      const query = {};
+      const cursor = eventsCollection.find(query);
+      const events = await cursor.toArray();
+      res.send(events);
+  })
+
+  }
+  finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("<center><h1> Human Care Server</h1></center>");
